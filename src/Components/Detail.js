c6bdda3722038377aa1d72cbdb5007c4b88a9da0
Laura Ningel <laura.ningel@gmail.com>
2020-10-15 15:40:34 +0200

    update routes

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -2,13 +2,19 @@ import React from 'react'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 
-export default function Detail({ richText, video }) {
+
+export default function Detail({ getRich, getVid }) {
+
+    console.log(getRich)
+
+    let vid = getVid()
+    console.log("video from detail", vid)
 
     return (
         <div className="container">
-            {richText}
+            {/* {rich} */}
 
-            <iframe width="560" height="315" src={video} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" height="315" src={vid} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,14 +1,32 @@
 import React from "react";
+import Detail from "./Detail.js";
+import { Link } from "react-router-dom"; 
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-export default function Main({ formatted, topics }) {
+
+export default function Main({ formatted, topics, match }) {
 
   const randomColor = () => {
     const colors = ["#F2EC7E", "#EB1D24", "#2F9AD3", "#E67192", "#F0901E", "#4BB13F", "#BC1F2E"]
     return colors[Math.floor(Math.random()*colors.length)]
   }
 
+  
+  const getRich = formatted
+  .filter(el => el.id === Number(match.params.id))
+  .map(el => documentToReactComponents(el.info))[0]
+
+  const getVid = () => {
+    const hi = formatted
+    .filter(el => el.id === Number(match.params.id)) // filter slugs instead
+    .map(el => el.video)[0]
+    return hi
+  }
+
+
   return (
-    <div className="container">
+    <>
+   {match.params.id ? <Detail getVid={() => getVid()} getRich={getRich}/> :  <div className="container">
       <select className="custom-select custom-select-lg mb-3">
         <option selected>Select spiceyness of your project</option>
         <option value="easy">🌶</option>
@@ -35,17 +53,27 @@ export default function Main({ formatted, topics }) {
                 <div className="card-body">
                   <h5 className="card-title">{el.title}</h5>
                   <p className="card-text">{el.abstract}</p>
-                  <a href="#" className="btn btn-warning" id={el.id} onClick={(e) => console.log(e.target.id)}>
+                  {/* <a href="#" className="btn btn-warning" id={el.id} onClick={(e) => console.log(e.target.id)}>
                     see project
-                  </a>
+                  </a> */}
+
+                  <Link to={`/projects/${el.id}`} className="btn btn-warning">
+                    see project
+                  </Link>
+
                 </div>
               </div>
             </div>
           );
         })}
       </div>
-    </div>
+    </div>}
+    </>
   );
+  
+
+
+    
 }
 
 
