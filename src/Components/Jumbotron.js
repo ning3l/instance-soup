4dff0bda7c8dc6ba018551e3759e31c041ed623f
Raphael Hemme <hemme.raphael@gmail.com>
2020-10-15 20:12:54 +0200

    Fix some bootstrap layout issues

--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -5,7 +5,8 @@ export default function Jumbotron() {
         <div className="jumbotron jumbotron-fluid">
         <div className="container">
             <h1 className="display-4"><span className="firstPart">Insta</span><span className="secondPart">(nce)<br/></span><span className="firstPart">Soup</span></h1>
-            <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            <p className="lead">Hungry for the next coding project? Instance Soup provides you with nourishing app-ideas you can get up and running in no time. Like the carbon based instance soup you know and love, it serves you all the ingredients conveniently. We call it "Tho whole stack in one pack".</p>
+            <p className="lead">Mr. E. (for Edamame) already loves it. <strong>And You can learn to love it too!</strong></p>
         </div>
         </div>
     )
